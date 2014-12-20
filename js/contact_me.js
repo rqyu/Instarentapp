c4ddaebaf1c26d696df052f7ee6bbc75d150b988
Ray <yrq901207@gmail.com>
2014-12-20 05:16:51 -0500

    Completion

--- js/contact_me.js
+++ js/contact_me.js
@@ -4,6 +4,7 @@ $(function() {
         preventSubmit: true,
         submitError: function($form, event, errors) {
             // additional error messages or events
+            console.log('wut');
         },
         submitSuccess: function($form, event) {
             event.preventDefault(); // prevent default submit behaviour
@@ -11,6 +12,9 @@ $(function() {
             var name = $("input#name").val();
             var email = $("input#email").val();
             var phone = $("input#phone").val();
+            var term = $("input#term").val();
+            var location = $("input#location").val();
+            var price = $("input#price").val();
             var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
@@ -18,13 +22,16 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "http://199.195.141.236/instarentappREAD.php",
                 type: "POST",
                 data: {
                     name: name,
                     phone: phone,
                     email: email,
-                    message: message
+                    message: message,
+                    term: term,
+                    location: location,
+                    price: price
                 },
                 cache: false,
                 success: function() {
@@ -33,7 +40,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Your application has been received. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -46,6 +53,7 @@ $(function() {
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append(error);
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
